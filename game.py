79e55cfd93802264b5bb41e7427d0130916c5d5a
UnderAnder <andryushin.alexander@gmail.com>
2021-03-12 19:05:03 +0700

    4/5 base rating

--- game.py
+++ game.py
@@ -8,13 +8,17 @@ class Rps:
                    'paper': 'scissors',
                    'scissors': 'rock'}
 
+    def __init__(self, user):
+        self.user = user
+
     def game(self, player_choice: str):
         comp_choise = random.choice(self.options)
-        result = ''
 
         if player_choice == comp_choise:
+            self.user.add_score(50)
             result = 'draw'
         elif player_choice == self.rules[comp_choise]:
+            self.user.add_score(100)
             result = 'win'
         else:
             result = 'lose'
@@ -30,11 +34,41 @@ class Rps:
         if result == 'win':
             print(f'Well done. The computer chose {option} and failed')
 
+class Users:
+    def __init__(self, name: str):
+        if not name:
+            print('Invalid name')
+            exit()
+        self.name = name
+        self.score = self.get_score()
+
+        print(f'Hello, {self.name}')
+
+    def get_score(self) -> int:
+        with open('rating.txt', 'r') as file:
+            for line in self.generate_lines_start_with(self.name, file):
+                return int(line.removeprefix(self.name))
+        return 0
+
+    def add_score(self, score):
+        self.score += score
+
+    @staticmethod
+    def generate_lines_start_with(string, file):
+        for line in file:
+            if line.startswith(string):
+                yield line
+
 def main():
+    user_name = input('Enter your name:')
+    user = Users(user_name)
+
     while True:
-        player_choice = input().strip()
+        player_choice = input('Pick one: rock, paper, scissors:').strip()
         if player_choice in Rps.options:
-            Rps().game(player_choice)
+            Rps(user).game(player_choice)
+        elif player_choice == '!rating':
+            print(f'Your rating: {user.score}')
         elif player_choice == '!exit':
             print('Bye!')
             exit()
